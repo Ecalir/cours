9c2a789b45eb2015492a6aa3503170753bc6a584
jeremysanna <jeremysanna49@gmail.com>
2021-02-09 16:22:03 +0100

    Ajout has_room_for et action_add pour la class Box

--- tp2.py
+++ tp2.py
@@ -4,24 +4,42 @@ class Box:
         self._ouvert = False
         self._capacity = None
 
+    # ===============
+    # Get
+    # ===============
+
     def __contains__(self,valeur):
         return valeur in self._contents
+
+    def capacity(self):
+        return self._capacity
+    
+    def is_open(self):
+        return self._ouvert
+
+    # ===============
+    # Set
+    # ===============
 
     def add(self, truc):
         self._contents.append(truc)
     
     def remove(self,truc):
         self._contents.remove(truc)
-    
-    def is_open(self):
-        return self._ouvert
-    
+
     def close(self):
         self._ouvert = False
     
     def open(self):
         self._ouvert = True
     
+    def set_capacity(self,nouveau_volume):
+        self._capacity = nouveau_volume
+
+    # ===============
+    # Méthodes
+    # ===============
+
     def action_look(self):
         resultat = "La boite contient: "
         if self._ouvert == True:
@@ -30,11 +48,24 @@ class Box:
             resultat = "La boite est fermée."
         return resultat
 
-    def set_capacity(self,nouveau_volume):
-        self._capacity = nouveau_volume
-    
-    def capacity(self):
-        return self._capacity
+    def has_room_for(self,objet):
+        if self._capacity == None or self._capacity >= objet.volume():
+            resultat = True
+        else:
+            resultat = False
+        return resultat
+
+    def action_add(self,valeur):
+        resultat = True
+        if self._capacity == None:
+            self.add(valeur)
+        elif self._capacity >= valeur.volume():
+            self.add(valeur)
+            self._capacity -= valeur.volume()
+        else:
+            resultat = False
+        return resultat
+
 
 class Thing:
     def __init__(self,volume):
